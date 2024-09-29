b5dcea53e8f1b34b1ac59099612075cdf0ba0420
Chitmin <aungchitmin135@gmail.com>
2024-09-29 10:20:54 +0630

    add gql client

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import './App.css'
+import { ApolloProvider } from "@apollo/client";
+import client from "./lib/client";
+import "./App.css"
 
 export default function App() {
-  return (
-    <h1 className="text-3xl font-bold">
-      Hello world!
-    </h1>
-  )
+  return <ApolloProvider client={client}>
+    <h1 className="text-3xl font-bold">Search</h1>
+    </ApolloProvider>
 }
