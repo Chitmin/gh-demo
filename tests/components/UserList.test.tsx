a755ec32974df9dbdd1838e509c4f37a5bbf8197
Chitmin <aungchitmin135@gmail.com>
2024-09-30 23:30:19 +0630

    minor fix

--- tests/components/UserList.test.tsx
+++ tests/components/UserList.test.tsx
@@ -1,5 +1,5 @@
-import { render, queryByAttribute } from '@testing-library/react'
-import { describe, it, expect } from 'vitest';
+import { render, queryByAttribute } from "@testing-library/react";
+import { describe, it, expect } from "vitest";
 import UserList from "../../src/components/UserList";
 import { UserContext } from "../../src/Contexts";
 
@@ -7,11 +7,11 @@ const defaultContextValue = {
   users: [],
   setUsers: () => {},
   selectUser: null,
-  setSelectUser: () => {}
+  setSelectUser: () => {},
 };
 
-describe('User List component', () => {
-  it('renders without error', () => {
+describe("User List component", () => {
+  it("renders without error", () => {
     render(
       <UserContext.Provider value={defaultContextValue}>
         <UserList />
@@ -19,37 +19,40 @@ describe('User List component', () => {
     );
   });
 
-  it('shows empty string for empty result', async () => {
+  it("shows empty string for empty result", async () => {
     const dom = render(
       <UserContext.Provider value={defaultContextValue}>
         <UserList />
       </UserContext.Provider>
     );
 
-    const getById = queryByAttribute.bind(null, 'id');
-    const section = getById(dom.container, 'user-list');
+    const getById = queryByAttribute.bind(null, "id");
+    const section = getById(dom.container, "user-list");
     expect(section).not.toBeNull();
-    
-    const ul = getById(dom.container, 'users');
+
+    const ul = getById(dom.container, "users");
     expect(ul).toBeNull();
   });
 
-  it('shows users', async () => {
+  it("shows users", async () => {
     const user = {
       id: "abcd",
       login: "example",
       avatarUrl: "http://example.com",
-      url: "http://example.com/example"
+      url: "http://example.com/example",
+      __typename: "User",
     };
 
     const dom = render(
       <UserContext.Provider value={{ ...defaultContextValue, users: [user] }}>
         <UserList />
       </UserContext.Provider>
-  );
+    );
 
-    const a = await dom.container.querySelector('#users > li > span:last-child');
+    const a = await dom.container.querySelector(
+      "#users > li > span:last-child"
+    );
     expect(a).not.toBeNull();
-    expect(a).toHaveTextContent('example');
+    expect(a).toHaveTextContent("example");
   });
-})
+});
