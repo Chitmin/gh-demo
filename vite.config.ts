5f60f5831fa6ad858a75fdae758920b8f889e297
Chitmin <aungchitmin135@gmail.com>
2024-10-01 00:08:54 +0630

    add docker and update readme

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react()],
   test: {
     include: ["./tests/**/*.{test,spec}.?(c|m)[jt]s?(x)"],
